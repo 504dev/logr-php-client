3f7b3ce58ace988d735b58b7afbd2c67b4a61d3f
Mona Lisa <nikruzhok@gmail.com>
2020-10-04 16:59:16 +0300

    composer.json

--- src/AES.php
+++ src/AES.php
@@ -1,5 +1,5 @@
 <?php
-define('AES_256_CFB', 'aes-256-cfb');
+define('AES_128_CFB', 'aes-128-cfb');
 
 class AES
 {
@@ -9,20 +9,20 @@ class AES
     function __construct($encryption_key)
     {
         $this->encryption_key = $encryption_key;
-        $this->block_size = openssl_cipher_iv_length(AES_256_CFB);
+        $this->block_size = openssl_cipher_iv_length(AES_128_CFB);
     }
 
     function encrypt(string $data)
     {
         $iv = openssl_random_pseudo_bytes($this->block_size);
-        $encrypted = openssl_encrypt($data, AES_256_CFB, $this->encryption_key, 0, $iv);
-        return base64_encode($iv . base64_decode($encrypted));
+        $encrypted = openssl_encrypt($data, AES_128_CFB, $this->encryption_key, 1, $iv);
+        return base64_encode($iv . $encrypted);
     }
 
     function decrypt(string $data)
     {
         $iv = base64_encode(substr(base64_decode($data), 0, $this->block_size));
         $enc = base64_encode(substr(base64_decode($data), $this->block_size, strlen($data) - $this->block_size));
-        return openssl_decrypt($enc, AES_256_CFB, $this->encryption_key, 0, base64_decode($iv));
+        return openssl_decrypt($enc, AES_128_CFB, $this->encryption_key, 0, base64_decode($iv));
     }
 }
